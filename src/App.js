3ed8e62e1360f8886510d74114a09e974fef0237
blackhat-user <136855749+blackhat-user@users.noreply.github.com>
2023-09-11 03:37:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './JSX/Header/header'
+import Main from './JSX/Main/main'
+import Footer from './JSX/footer/'
+import './Sass/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/JSX/Header/header.js
@@ -0,0 +1,11 @@
+import '/React version/app/src/Sass/components/header.css'
+
+const Header = () => {
+    return (
+        <header class='header__wrapper'>
+            
+        </header>
+    );
+}
+
+export default Header;
